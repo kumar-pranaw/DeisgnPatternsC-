57a3f78d9c108188c0c3a8edff580f8f724104bd
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-07-20 14:41:17 +0530

    Single responsibliity Principle Any Particular class have single reason to change

--- SolidPrinciples/Program.cs
+++ SolidPrinciples/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SolidPrinciples
 {
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}:{text}");
+            return count;// memento
+        }
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if(overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, j.ToString());
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var j = new Journal();
+            j.AddEntry("I cried Today");
+            j.AddEntry("I ate a bug");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"C:\Users\pranav.k.verma\Documents\journal.txt";
+            p.SaveToFile(j, filename);
+            Process.Start(filename);
+           
         }
     }
 }
