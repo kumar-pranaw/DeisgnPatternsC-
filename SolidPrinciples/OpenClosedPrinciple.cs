db323712830766241dce3059336c496a0d03f413
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-07-20 16:27:47 +0530

    Renamed Classes

---
+++ SolidPrinciples/OpenClosedPrinciple.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolidPrinciples
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public enum Size
+    {
+        Small, Medium, Large, Yuge
+    }
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+            Name = name;
+            Color = color;
+            Size = size;
+
+        }
+    }
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size)
+                {
+                    yield return p;
+                }
+            }
+        }
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color)
+                {
+                    yield return p;
+                }
+            }
+        }
+    }
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+    public class ColorSpecification: ISpecification<Product>
+    {
+        private Color color;
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(first));
+            }
+            if(second== null)
+            {
+                throw new ArgumentNullException(paramName: nameof(second));
+            }
+            this.first = first?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+           foreach(var i in items)
+            {
+                if (spec.IsSatisfied(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+    public class OpenClosedPrinciple
+    {
+
+    }
+}
--- SolidPrinciples/SingleResponsibility.cs
+++ SolidPrinciples/SingleResponsibility.cs
@@ -1,124 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SolidPrinciples
 {
-    public enum Color
+    public class Journal
     {
-        Red, Green, Blue
-    }
-    public enum Size
-    {
-        Small, Medium, Large, Yuge
-    }
-    public class Product
-    {
-        public string Name;
-        public Color Color;
-        public Size Size;
-        public Product(string name, Color color, Size size)
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+        public int AddEntry(string text)
         {
-            if (name == null)
+            entries.Add($"{++count}:{text}");
+            return count;// memento
+        }
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
             {
-                throw new ArgumentNullException(paramName: nameof(name));
+                File.WriteAllText(filename, j.ToString());
             }
-            Name = name;
-            Color = color;
-            Size = size;
+        }
+    }
+    class SingleResponsibility
+    {
+        static void Main(string[] args)
+        {
 
-        }
-    }
-    public class ProductFilter
-    {
-        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
-        {
-            foreach (var p in products)
-            {
-                if (p.Size == size)
-                {
-                    yield return p;
-                }
-            }
-        }
-        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
-        {
-            foreach (var p in products)
-            {
-                if (p.Color == color)
-                {
-                    yield return p;
-                }
-            }
-        }
-    }
-    public interface ISpecification<T>
-    {
-        bool IsSatisfied(T t);
-    }
-    public interface IFilter<T>
-    {
-        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
-    }
-    public class ColorSpecification: ISpecification<Product>
-    {
-        private Color color;
-        public ColorSpecification(Color color)
-        {
-            this.color = color;
-        }
-        public bool IsSatisfied(Product t)
-        {
-            return t.Color == color;
-        }
-    }
-    public class SizeSpecification : ISpecification<Product>
-    {
-        private Size size;
-        public SizeSpecification(Size size)
-        {
-            this.size = size;
-        }
-        public bool IsSatisfied(Product t)
-        {
-            return t.Size == size;
-        }
-    }
-    public class AndSpecification<T> : ISpecification<T>
-    {
-        private ISpecification<T> first, second;
-        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
-        {
-            if (first == null)
-            {
-                throw new ArgumentNullException(paramName: nameof(first));
-            }
-            if(second== null)
-            {
-                throw new ArgumentNullException(paramName: nameof(second));
-            }
-            this.first = first?? throw new ArgumentNullException(paramName: nameof(first));
-            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
-        }
-        public bool IsSatisfied(T t)
-        {
-            return first.IsSatisfied(t) && second.IsSatisfied(t);
-        }
-    }
-    public class BetterFilter : IFilter<Product>
-    {
-        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
-        {
-           foreach(var i in items)
-            {
-                if (spec.IsSatisfied(i))
-                {
-                    yield return i;
-                }
-            }
-        }
-    }
-    public class SingleResponsibility
-    {
+            #region Code for First Approach
+            //var j = new Journal();
+            //j.AddEntry("I cried Today");
+            //j.AddEntry("I ate a bug");
+            //Console.WriteLine(j);
 
+            //var p = new Persistence();
+            //var filename = @"C:\Users\pranav.k.verma\Documents\journal.txt";
+            //p.SaveToFile(j, filename);
+            //Process.Start(filename);
+
+            #endregion
+
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+            Product[] products = { apple, tree, house };
+            var pf = new ProductFilter();
+            Console.WriteLine("Green Products (Old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
+            var bf = new BetterFilter();
+            Console.WriteLine("Green Products (new):");
+            foreach(var p in bf.Filter(products, new ColorSpecification(Color.Blue))){
+                Console.WriteLine($" - {p.Name} is green");
+            }
+
+            Console.WriteLine("Large Blue Item: ");
+            foreach(var p in bf.Filter(products,new AndSpecification<Product>(
+                                                 new ColorSpecification(Color.Blue),
+                                                 new SizeSpecification(Size.Large))
+                                                    ))
+            {
+                Console.WriteLine($" - { p.Name} is big and blue");
+            }
+        }
     }
 }
