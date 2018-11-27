f1e0130eb4673cf1297bee000933297813bf5940
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-11-27 15:49:20 +0530

    Added Point Class Also we are trying to make a situation where we can use factory method

---
+++ FactoryPattern/Point.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+    class Point
+    {
+        private double x, y;
+        /// <summary>
+        /// Initializes a point from Either cartesian or polar
+        /// </summary>
+        /// <param name="a"> x if Cartesian rho if polar</param>
+        /// <param name="b"></param>
+        /// <param name="system"></param>
+        public Point(double a, double b, CoordinateSystem system = CoordinateSystem.Cartesian)
+        {
+            switch (system)
+            {
+                case CoordinateSystem.Cartesian:
+                    x = a;
+                    y = b;
+                    break;
+                case CoordinateSystem.Polar:
+                    x = a * Math.Cos(b);
+                    y = b * Math.Sin(b);
+                    break;
+                default:
+                    break;
+            }
+        }
+ 
+        static void Main(string[] args)
+        {
+        }
+    }
+}
